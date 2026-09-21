464a4bb9ed2769f7b4b8b7678e532d5c17147488
costacristina1-ai <costa.cristina1@aluno.ifsp.edu.br>
2026-09-20 22:49:44 -0300

    Add files via upload

---
+++ O_Cofre_Carmesim/assets/README_THEME.txt
@@ -0,0 +1,8 @@
+Tema do projeto: O Cofre Carmesim.
+
+Paleta:
+- Bege: #E8D5B5 / #F4E7D0
+- Vermelho: #8F2F2A / #7A241F
+- Marrom avermelhado: #3A1715 / #5A1F1B
+
+A imagem background.png foi adaptada para o tema do cofre.
---
+++ O_Cofre_Carmesim/constants/cores.js
@@ -0,0 +1,16 @@
+const Cores = {
+  backgroundDark: '#3A1715',
+  background: '#5A1F1B',
+  primary500: '#8F2F2A',
+  primary600: '#7A241F',
+  primary700: '#64201C',
+  primary800: '#421512',
+  accent500: '#E8D5B5',
+  accent600: '#C8A982',
+  accent700: '#A67C52',
+  cream: '#F4E7D0',
+  textDark: '#3A1715',
+  white: '#FFF8EE',
+};
+
+export default Cores;
